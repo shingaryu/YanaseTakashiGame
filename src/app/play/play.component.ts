f479829611c01bebf6b9b8d93d8c0d7c6fe21385
shingaryu <shingaryu@gmail.com>
2018-12-31 22:07:56 +0900

    Create the template of the quiz logic

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuizService } from '../quiz.service';
 
 @Component({
   selector: 'app-play',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayComponent implements OnInit {
 
-  constructor() { }
+  numberOfQuestions = 10; // can be modified
+  questions: string[];
+  currentUserAnswer: string; // binded to style
+  userAnswers: string[];
+  currentQuestionIndex: number;
+
+  constructor(private quizService: QuizService) { }
 
   ngOnInit() {
+  }
+
+  onStartClick() {
+    this.questions = this.quizService.randomQuestions(10);
+    this.currentQuestionIndex = 0;
+    this.userAnswers = [];
+    this.currentUserAnswer = '';
+  }
+
+  onNextQuestionClick() {
+    this.userAnswers.push(this.currentUserAnswer);
+    this.currentQuestionIndex++;
+
+    this.currentUserAnswer = '';
+    if (this.currentQuestionIndex >= this.numberOfQuestions) {
+      // finish
+      this.finishQuestion();
+    } else {
+      
+    }
+  }
+
+  finishQuestion() {
+
   }
 
 }
---
+++ src/app/quiz.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizService {
+
+  constructor() { }
+
+  public randomQuestions(num: number) {
+    const questions = [];
+    for (let i = 0; i < num; i++) {
+      questions.push(this.randomQuestion()); 
+    }
+
+    return questions;
+  }
+
+  public randomQuestion(): string {
+    return null;
+  }
+
+  public answer(question: string): string {
+    return null;
+  }
+}
