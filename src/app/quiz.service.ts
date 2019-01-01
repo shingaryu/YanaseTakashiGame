dcd4cab3018dba54d22cbf826f9c619ec5608697
shingaryu <shingaryu@gmail.com>
2019-01-01 17:31:30 +0900

    Create answer function and question examples

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -5,6 +5,24 @@ import { Injectable } from '@angular/core';
 })
 export class QuizService {
 
+  examples = [
+    'やなたせいし',
+    'セYAナ夕力',
+    'ヤナセya',
+    'せやな大学',
+    'やせたなお前',
+    'せっくすやな',
+    'やめさせない',
+    'やなせやな',
+    'ヤナｾタカツ',
+    '聖闘士せやな',
+    'SEのやなせ',
+    'やよいなせ',
+    'seいなYA',
+    'yanaせた',
+    'NAやせYA'
+  ]
+
   constructor() { }
 
   public randomQuestions(num: number) {
@@ -17,10 +35,24 @@ export class QuizService {
   }
 
   public randomQuestion(): string {
-    return 'やなたせいし'; // temporary
+    return this.examples[Math.floor(Math.random() * this.examples.length)]; // temporary
   }
 
   public answer(question: string): string {
-    return 'たかたしいし'; // temporary
+    const replaced = question.replace('せやな', '大学')
+      .replace('や', 'た')
+      .replace('ヤ', 'タ')
+      .replace('YA', 'TA')
+      .replace('ya', 'ta')
+      .replace('な', 'か')
+      .replace('ナ', 'カ')
+      .replace('NA', 'KA')
+      .replace('na', 'ka')
+      .replace('せ', 'し')
+      .replace('セ', 'シ')
+      .replace('SE', 'SHI')
+      .replace('se', 'shi')
+
+    return replaced;
   }
 }
